bea9626980bd24555b82f43bec25e3f332e4c68d
Ammar Jabakji <ammarjabakji@gmail.com>
2019-12-12 16:02:59 +0300

    Counter Example

--- src/App.js
+++ src/App.js
@@ -2,20 +2,10 @@ import React from 'react'
 import './App.css'
 import { Example } from './components/example'
 
-const Lake = ({ name }) => <h1>{name}</h1>
-const Lakes = () => (
-  <div>
-    <Lake name="lake Tueje" />
-    <Lake name="lake Kufoe" />
-    <Lake name="lake Zirha" />
-  </div>
-)
-
 function App() {
   return (
     <div className="App">
-      <h1>Example 1</h1>
-      <Lakes />
+      <h1>Example 2</h1>
       <Example />
     </div>
   )
