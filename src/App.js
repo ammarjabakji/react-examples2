56486f1600c77eec60f8a40830bac474ab2b3710
Ammar Jabakji <ammarjabakji@gmail.com>
2019-12-25 16:40:41 +0300

    adding form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import { GithubCards } from './components/githubCards'
 
-function App() {
-  return (
-    <div className="App">
-      <GithubCards title="The GitHub Cards App" />
-    </div>
-  )
-}
+const testData = [
+  {
+    name: 'Dan Abramov',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
+    company: '@facebook'
+  },
+  {
+    name: 'Sophie Alpert',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/6820?v=4',
+    company: 'Humu'
+  },
+  {
+    name: 'Sebastian Markbåge',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/63648?v=4',
+    company: 'Facebook'
+  }
+]
 
-export default App
+export default class App extends Component {
+  state = {
+    testData
+  }
+  render() {
+    return (
+      <div className="App">
+        <GithubCards
+          testData={this.state.testData}
+          title="The GitHub Cards App"
+        />
+      </div>
+    )
+  }
+}
--- src/components/githubCards.js
+++ src/components/githubCards.js
@@ -1,26 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const testData = [
-  {
-    name: 'Dan Abramov',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
-    company: '@facebook'
-  },
-  {
-    name: 'Sophie Alpert',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/6820?v=4',
-    company: 'Humu'
-  },
-  {
-    name: 'Sebastian Markbåge',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/63648?v=4',
-    company: 'Facebook'
-  }
-]
-
-const CardList = () => (
+const CardList = ({ profiles }) => (
   <div>
-    {testData.map(profile => (
+    {profiles.map(profile => (
       // <Card
       //   name={profile.name}
       //   avatar_url={profile.avatar_url}
@@ -33,37 +15,48 @@ const CardList = () => (
   </div>
 )
 
-// class Card extends React.Component {
-//   render() {
-//     const profile = this.props
-//     return (
-//       <div className="github-profile">
-//         <img src={profile.avatar_url} alt="" />
-//         <div className="info">
-//           <div className="name">{profile.name}</div>
-//           <div className="company">{profile.company}</div>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
+class Card extends Component {
+  render() {
+    // const profile = this.props
+    //or more professional code
+    const { avatar_url, name, company } = this.props
+    return (
+      <div className="github-profile">
+        <img src={avatar_url} alt="" />
+        <div className="info">
+          <div className="name">{name}</div>
+          <div className="company">{company}</div>
+        </div>
+      </div>
+    )
+  }
+}
 
 // this code is using functional components and much simpler than the above code
-const Card = ({ name, avatar_url, company }) => (
-  <div className="github-profile">
-    <img src={avatar_url} alt="" />
-    <div className="info">
-      <div className="name">{name}</div>
-      <div className="company">{company}</div>
-    </div>
-  </div>
-)
+// const Card = ({ name, avatar_url, company }) => (
+//   <div className="github-profile">
+//     <img src={avatar_url} alt="" />
+//     <div className="info">
+//       <div className="name">{name}</div>
+//       <div className="company">{company}</div>
+//     </div>
+//   </div>
+// )
+const Form = () => {
+  return (
+    <form action="">
+      <input type="text" placeholder="GitHub username" />
+      <button>Add card</button>
+    </form>
+  )
+}
 
-export const GithubCards = ({ title }) => {
+export const GithubCards = ({ title, testData }) => {
   return (
     <div>
       <div className="header">{title}</div>
-      <CardList />
+      <Form />
+      <CardList profiles={testData} />
     </div>
   )
 }
