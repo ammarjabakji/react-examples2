6a48c3a4d830273966cfc3e162423de2f7224425
Ammar Jabakji <ammarjabakji@gmail.com>
2019-12-30 14:34:51 +0300

    rollingback

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import { GithubCards } from './components/githubCards'
 
-export const App = () => {
-  const [data, setData] = useState({
-    id: '',
-    name: '',
-    avatar_url: '',
-    company: ''
-  })
-  return (
-    <div className="App">
-      <GithubCards
-        testData={this.state.testData}
-        onSubmit={this.addNewProfile}
-        title="The GitHub Cards App"
-      />
-    </div>
-  )
+const testData = [
+  {
+    id: '1',
+    name: 'Dan Abramov',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
+    company: '@facebook'
+  },
+  {
+    id: '2',
+    name: 'Sophie Alpert',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/6820?v=4',
+    company: 'Humu'
+  },
+  {
+    id: '3',
+    name: 'Sebastian Markbåge',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/63648?v=4',
+    company: 'Facebook'
+  }
+]
+
+export default class App extends Component {
+  state = {
+    testData
+  }
+  addNewProfile = profileData => {
+    this.setState(prevState => ({
+      testData: [...prevState.testData, profileData]
+    }))
+  }
+  render() {
+    return (
+      <div className="App">
+        <GithubCards
+          testData={this.state.testData}
+          onSubmit={this.addNewProfile}
+          title="The GitHub Cards App"
+        />
+      </div>
+    )
+  }
 }
