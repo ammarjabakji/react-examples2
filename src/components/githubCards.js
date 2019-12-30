44716a0a0a3bbb10bf1c738ceb8b2c189017fa3e
Ammar Jabakji <ammarjabakji@gmail.com>
2019-12-30 14:10:34 +0300

    adding useState

--- src/components/githubCards.js
+++ src/components/githubCards.js
@@ -4,45 +4,20 @@ import axios from 'axios'
 const CardList = ({ profiles }) => (
   <div>
     {profiles.map(profile => (
-      // <Card
-      //   name={profile.name}
-      //   avatar_url={profile.avatar_url}
-      //   company={profile.company}
-      // />
-
-      // this code is equivalent to the above
       <Card key={profile.id} {...profile} />
     ))}
   </div>
 )
 
-class Card extends Component {
-  render() {
-    // const profile = this.props
-    //or more professional code
-    const { avatar_url, name, company } = this.props
-    return (
-      <div className="github-profile">
-        <img src={avatar_url} alt="" />
-        <div className="info">
-          <div className="name">{name}</div>
-          <div className="company">{company}</div>
-        </div>
-      </div>
-    )
-  }
-}
-
-// this code is using functional components and much simpler than the above code
-// const Card = ({ name, avatar_url, company }) => (
-//   <div className="github-profile">
-//     <img src={avatar_url} alt="" />
-//     <div className="info">
-//       <div className="name">{name}</div>
-//       <div className="company">{company}</div>
-//     </div>
-//   </div>
-// )
+const Card = ({ name, avatar_url, company }) => (
+  <div className="github-profile">
+    <img src={avatar_url} alt="" />
+    <div className="info">
+      <div className="name">{name}</div>
+      <div className="company">{company}</div>
+    </div>
+  </div>
+)
 class Form extends Component {
   state = { userName: '' }
   handleSubmit = async event => {
